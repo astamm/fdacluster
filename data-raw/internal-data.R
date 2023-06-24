50e0377da8f389274146b73ea0d06df6135bc8d1
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2023-06-24 16:38:21 +0200

    Added vignette on initialization strategies for kmeans.

--- data-raw/internal-data.R
+++ data-raw/internal-data.R
@@ -26,11 +26,90 @@ phase_data <- compare_caps(
   cluster_on_phase = TRUE
 )
 
+out_manual <- fdakmeans(
+  x = simulated30$x,
+  y = simulated30$y,
+  n_clusters = 2,
+  seeds = c(1, 21),
+  warping_class = "affine",
+  centroid_type = "mean",
+  metric = "l2",
+  cluster_on_phase = FALSE,
+  use_verbose = FALSE
+)
+
+withr::with_seed(1234, {
+  initial_seeds <- replicate(10, sample.int(30, 2, replace = FALSE), simplify = FALSE)
+  outs_manual <- purrr::map(initial_seeds, \(.seeds) {
+    fdakmeans(
+      x = simulated30$x,
+      y = simulated30$y,
+      n_clusters = 2,
+      seeds = .seeds,
+      warping_class = "affine",
+      centroid_type = "mean",
+      metric = "l2",
+      cluster_on_phase = FALSE,
+      use_verbose = FALSE
+    )
+  })
+})
+
+withr::with_seed(1234, {
+  outs_kpp <- replicate(10, {
+    fdakmeans(
+      x = simulated30$x,
+      y = simulated30$y,
+      n_clusters = 2,
+      seeding_strategy = "kmeans++",
+      warping_class = "affine",
+      centroid_type = "mean",
+      metric = "l2",
+      cluster_on_phase = FALSE,
+      use_verbose = FALSE
+    )
+  }, simplify = FALSE)
+})
+
+withr::with_seed(1234, {
+  outs_ekpp <- replicate(10, {
+    fdakmeans(
+      x = simulated30$x,
+      y = simulated30$y,
+      n_clusters = 2,
+      seeding_strategy = "exhaustive-kmeans++",
+      warping_class = "affine",
+      centroid_type = "mean",
+      metric = "l2",
+      cluster_on_phase = FALSE,
+      use_verbose = FALSE
+    )
+  }, simplify = FALSE)
+})
+
+out_hclust <- fdakmeans(
+  x = simulated30$x,
+  y = simulated30$y,
+  n_clusters = 2,
+  seeding_strategy = "hclust",
+  warping_class = "affine",
+  centroid_type = "mean",
+  metric = "l2",
+  cluster_on_phase = FALSE,
+  use_verbose = FALSE
+)
+
 plan(sequential)
 
 usethis::use_data(
   amplitude_data,
   phase_data,
+  initial_seeds,
+  out_manual,
+  outs_manual,
+  outs_kpp,
+  outs_ekpp,
+  out_hclust,
   internal = TRUE,
   overwrite = TRUE,
   compress = "xz",
